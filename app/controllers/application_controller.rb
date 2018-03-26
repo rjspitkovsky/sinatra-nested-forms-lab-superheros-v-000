562497625f5d2baece8692509a690a858dd06a34
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-03-26 14:21:46 -0400

    Solution

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,21 @@
 require 'sinatra/base'
+require './config/environment'
 
 class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
     get '/' do
-      erb :super_hero  
+      erb :super_hero
+    end
+
+    post '/teams' do
+      @team = Team.new(params[:team])
+      params[:team][:heroes].each do |hero_details|
+        Hero.new(hero_details)
+      end
+      @heroes = Hero.all
+      erb :team
     end
 
 
--- app/models/team.rb
+++ app/models/team.rb
@@ -7,10 +7,11 @@ class Team
   def initialize(params)
     @name = params[:name]
     @motto = params[:motto]
+    @@all << self 
   end
 
   def self.all
     @@all
-  end 
+  end
 
 end
