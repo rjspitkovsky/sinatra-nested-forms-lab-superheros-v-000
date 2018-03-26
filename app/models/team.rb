846871e5e193a1954919c41410fb2d56d7842b08
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-03-26 13:50:06 -0400

    team.rb work

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,16 @@
 class Team
 
-end 
+  attr_accessor :name, :motto
+
+  @@all = []
+
+  def initialize(params)
+    @name = params[:name]
+    @motto = params[:motto]
+  end
+
+  def self.all
+    @@all
+  end 
+
+end
